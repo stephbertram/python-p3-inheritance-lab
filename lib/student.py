eb3b4be7e9354c072b274a5bb093ed81378161df
Stephanie Bertram <steph.a.bertram@gmail.com>
2024-03-17 16:40:52 -0700

    complete lab

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,10 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    knowledge = []
+    
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+    
+    def learn(self, knowledge_string):
+        self.knowledge.append(knowledge_string)
